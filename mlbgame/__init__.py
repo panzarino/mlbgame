22a5bfee9f0dfd3e2aaebc287c9fcb1984f78952
Zach Panzarino <zachary@panzarino.com>
2015-11-03 20:51:02 +0000

    Ensure it is only run with python 2.6+

---
+++ mlbgame/__init__.py
@@ -0,0 +1,6 @@
+import sys
+
+if sys.version_info[0] != 2:
+	print("mlbgame requires Python 2.6+ and does not work with Python 3")
+	print("You are running Python version {}.{}".format(sys.version_info.major, sys.version_info.minor))
+	sys.exit(1)
