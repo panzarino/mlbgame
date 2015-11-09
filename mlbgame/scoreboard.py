0d6e90030270a1e8a2497f05d227d03c3717f3cd
Zach Panzarino <zachary@panzarino.com>
2015-11-09 18:39:05 +0000

    Some statistics, not finished

--- mlbgame/scoreboard.py
+++ mlbgame/scoreboard.py
@@ -2,6 +2,8 @@ import urllib2 as url
 import xml.etree.ElementTree as etree
 
 def scoreboard(year, month, day):
+    monthstr = str(month)
+    daystr = str(day)
     if month < 10:
         monthstr = "0"+str(month)
     if day < 10:
@@ -11,5 +13,14 @@ def scoreboard(year, month, day):
     root = data.getroot()
     games = []
     for game in root:
-        games.append(game)
-    return games
+        game_data = game.find('game')
+        game_id = game_data.attrib['id']
+        game_league = game_data.attrib['league']
+        game_status = game_data.attrib['status']
+        game_start_time = game_data.attrib['start_time']
+        teams = game.findall('team')
+        home_team_data = teams[0].find('gameteam')
+        home_team = {'name': teams[0].attrib['name'], 'runs': home_team_data.attrib['R'], 'hits':home_team_data.attrib['H'], 'errors':home_team_data.attrib['E']}
+        away_team_data = teams[1].find('gameteam')
+        away_team = {'name': teams[1].attrib['name'], 'runs': away_team_data.attrib['R'], 'hits':away_team_data.attrib['H'], 'errors':away_team_data.attrib['E']}
+        
