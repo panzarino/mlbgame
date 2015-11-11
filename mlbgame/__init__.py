73b8dc2ea1162530a17e91272dae2fe083b7d1af
Zach Panzarino <zachary@panzarino.com>
2015-11-11 19:26:25 +0000

    Version

--- mlbgame/__init__.py
+++ mlbgame/__init__.py
@@ -4,3 +4,6 @@ if sys.version_info[0] != 2:
 	print("mlbgame requires Python 2.6+ and does not work with Python 3")
 	print("You are running Python version {}.{}".format(sys.version_info.major, sys.version_info.minor))
 	sys.exit(1)
+
+from mlbgame import version
+VERSION = version.__version__
