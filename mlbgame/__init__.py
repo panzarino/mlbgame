596a5c7dadbb3a19d04462d8fab5f7e95e823e50
Zach Panzarino <zachary@panzarino.com>
2015-11-19 21:53:15 +0000

    More documentation

--- mlbgame/__init__.py
+++ mlbgame/__init__.py
@@ -15,6 +15,37 @@ mlbgame will download the data from mlb.com.
 If the information you request is from completed games,
 that data will be cached to disk.
 
+[mlbgame documentation](http://zachpanz88.github.io/mlbgame)
+
+Installation
+------------
+
+mlbgame is in the [Python Package Index (PyPI)](http://pypi.python.org/pypi/mlbgame/).
+Installing with `pip` is recommended for all systems.
+
+mlbgame can be installed by running:
+
+    pip install mlbgame
+
+(You may need to use `pip2` if Python 3 is the default on your system.)
+
+mlbgame depends on `urllib2` to fetch data from mlb.com.
+'urllib2' will be automatically installed if mlbgame is installed with `pip`
+
+mlbgame does not yet work on Python 3, but it should work with Python 2.6 and 2.7.
+
+Updating the Game Database
+--------------------------
+
+Since games happen every day, new game data exists that is not stored on disk from the original install.
+The database can be updated by running the following command:
+
+    mlbgame-update-games
+
+
+Examples
+--------
+
 Here is a quick teaser to find the scores of all home Mets games for the month of June, 2015:
 
     #!python
