cc3ff5b0b5bd1f16280008c990dd27cd83590bf2
Zach Panzarino <zachary@panzarino.com>
2015-12-11 13:22:22 +0000

    Some stats in the idmap

--- mlbgame/statmap.py
+++ mlbgame/statmap.py
@@ -9,7 +9,52 @@ will contain each property.
 
 '''
 Stat identifiers and their meanings
+
+`obj` - the objects that contain the stat
+
+`always` - whether the stat is always present in those objects
+
+`desc` - description of the stat
 '''
 idmap = {
-    
+    'name_display_first_last':{
+        'obj': ['PitcherStats', 'BatterStats'],
+        'always': True,
+        'desc': 'First and last name of player',
+    },
+    'name':{
+        'obj': ['PitcherStats', 'BatterStats'],
+        'always': True,
+        'desc': 'Abbreviated name, Either "F. Lastname" or "Lastname"',
+    },
+    'pos':{
+        'obj': ['PitcherStats', 'BatterStats'],
+        'always': True,
+        'desc': 'Player\'s position',
+    },
+    'id':{
+        'obj': ['PitcherStats', 'BatterStats'],
+        'always': True,
+        'desc': 'Player\'s id number for mlb.com',
+    },
+    'ab':{
+        'obj': ['BatterStats'],
+        'always': True,
+        'desc': 'At bats',
+    },
+    'avg':{
+        'obj': ['BatterStats'],
+        'always': True,
+        'desc': 'Batting average',
+    },
+    'h':{
+        'obj': ['PitcherStats', 'BatterStats'],
+        'always': True,
+        'desc': 'Hits (pitcher: given up)',
+    },
+    'r':{
+        'obj': ['PitcherStats', 'BatterStats'],
+        'always': True,
+        'desc': 'Runs (pitcher: given up)',
+    },
 }
