07b70a85f8062060b02ee515ab75092d1a2d6261
Zach Panzarino <zachary@panzarino.com>
2016-01-05 19:25:13 +0000

    Add comments

--- mlbgame/update_games.py
+++ mlbgame/update_games.py
@@ -10,11 +10,14 @@ def run(hide=False, more=False, start="01-01-2012"):
     '''
     Update local game data
     '''
+    # get today's information
     year = date.today().year
     month = date.today().month
     day = date.today().day
+    # get starting date information
     start_month, start_day, start_year = start.split("-")
     first_day, first_month = [True, True]
+    # print a message becuase sometimes it seems like the program is not doing anything
     if not hide:
         print "Checking local data..."
     # looping years
@@ -42,51 +45,73 @@ def run(hide=False, more=False, start="01-01-2012"):
                 if i == year and x >= month and y >= day:
                     break
                 daystr = str(y).zfill(2)
+                # file information
                 filename = "gameday-data/year_%i/month_%s/day_%s/scoreboard.xml.gz" % (i, monthstr, daystr)
                 f = os.path.join(os.path.dirname(__file__), filename)
                 dirn = "gameday-data/year_%i/month_%s/day_%s" % (i, monthstr, daystr)
                 dirname = os.path.join(os.path.dirname(__file__), dirn)
+                # check if file exists
+                # aka is the data saved
                 if not os.path.isfile(f):
+                    # try becuase some dates may not have a file on the mlb.com server
+                    # or some months don't have a 31st day
                     try:
+                        # get data from url
                         data = url.urlopen("http://gd2.mlb.com/components/game/mlb/year_%i/month_%s/day_%s/scoreboard.xml" % (i, monthstr, daystr))
+                        # loding bar to show something is actually happening
                         if not hide:
                             sys.stdout.write('Loading games for %s-%d (%00.2f%%) \r' % (monthstr, i, y/31.0*100))
                             sys.stdout.flush()
                         loading = True
                         response = data.read()
+                        # check if the path exists where the file should go
                         if not os.path.exists(dirname):
                             try:
+                                # try to make the folder if permissions allow
                                 os.makedirs(dirname)
                             except OSError:
                                 print 'I do not have write access to "%s".' % (os.path.join(os.path.dirname(__file__), 'gameday-data/'))
                                 print 'Without write access, I cannot update the game database.'
                                 sys.exit(1)
                         try:
+                            # try to create the file if permissions allow
                             with gzip.open(f, "w") as fi:
                                 fi.write(response)
                         except OSError:
                             print 'I do not have write access to "%s".' % dirname
                             print 'Without write access, I cannot update the game database.'
                             sys.exit(1)
+                    # do nothing if the file is not on mlb.com
                     except url.HTTPError:
                         pass
+                # get extra data if specified
                 if more:
                     try:
+                        # get the data for games on this day
                         games = mlbgame.day(i, x, y)
                         for z in games:
+                            # get the game id which is used to fetch data
                             game_id = z.game_id
+                            # file information
                             filename2 = "gameday-data/year_%i/month_%s/day_%s/gid_%s/boxscore.xml.gz" % (i, monthstr, daystr, game_id)
                             f2 = os.path.join(os.path.dirname(__file__), filename2)
                             dirn2 = "gameday-data/year_%i/month_%s/day_%s/gid_%s" % (i, monthstr, daystr, game_id)
                             dirname2 = os.path.join(os.path.dirname(__file__), dirn2)
+                            # check if file exists
+                            # aka is the information saved
                             if not os.path.isfile(f2):
+                                # try becuase some dates may not have a file on the mlb.com server
+                                # or some months don't have a 31st day
                                 try:
+                                    # get data
                                     data2 = url.urlopen("http://gd2.mlb.com/components/game/mlb/year_%i/month_%s/day_%s/gid_%s/boxscore.xml" % (i, monthstr, daystr, game_id))
                                     if not hide:
+                                        # progress
                                         sys.stdout.write('Loading games for %s-%d (%00.2f%%). \r' % (monthstr, i, y/31.0*100))
                                         sys.stdout.flush()
                                     loading = True
                                     response2 = data2.read()
+                                    # checking if files exist and writing new files
                                     if not os.path.exists(dirname2):
                                         try:
                                             os.makedirs(dirname2)
@@ -106,6 +131,7 @@ def run(hide=False, more=False, start="01-01-2012"):
                     except:
                         pass
             if loading and not hide:
+                # make sure loading ends at 100%
                 sys.stdout.write('Loading games for %s-%d (100.00%%).\n' % (monthstr, i))
                 sys.stdout.flush()
     if not hide:
