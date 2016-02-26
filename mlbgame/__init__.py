5ab49ca76b606ae3f4f989f409407421e5889a9b
Zach Panzarino <zachary@panzarino.com>
2016-02-26 18:17:02 +0000

    Include language for documentation

--- mlbgame/__init__.py
+++ mlbgame/__init__.py
@@ -75,6 +75,7 @@ Examples
 
 Here is a quick teaser to find the scores of all home Mets games for the month of June, 2015:
 
+    #!python
     from __future__ import print_function
     import mlbgame
     
@@ -100,6 +101,7 @@ And the output is:
 
 Maybe you want to know the pitchers for the Royals game on April 30th, 2015:
 
+    #!python
     from __future__ import print_function
     import mlbgame
     
@@ -116,6 +118,7 @@ Finding stats for the Mets batters
 in the final game of the 2015 World Series
 can also be done:
 
+    #!python
     from __future__ import print_function
     import mlbgame
     
