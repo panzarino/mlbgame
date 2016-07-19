f4ff9a0cf479f6f7b2c3773b8e48f731ccbdf895
Zach Panzarino <zachary@panzarino.com>
2016-07-19 15:20:56 +0000

    Add object for storing overview data

--- mlbgame/game.py
+++ mlbgame/game.py
@@ -227,3 +227,21 @@ def overview(game_id):
     for x in root.attrib:
         output[x] = root.attrib[x]
     return output
+
+class Overview(object):
+    def __init__(self, data):
+        """Creates an overview object that matches the corresponding info in `data`.
+        
+        `data` should be an dictionary of values.
+        """
+        # loop through data
+        for x in data:
+            # set information as correct data type
+            try:
+                setattr(self, x, int(data[x]))
+            except ValueError:
+                try:
+                    setattr(self, x, float(data[x]))
+                except ValueError:
+                    # string if not number
+                    setattr(self, x, str(data[x]))
