cd96c60c84ffdaed7ebe1ee019cb38f50b7a90ad
Zach Panzarino <zachary@panzarino.com>
2016-11-09 16:07:09 +0000

    Add properties to info documentation

--- mlbgame/info.py
+++ mlbgame/info.py
@@ -38,7 +38,67 @@ def team_info():
     return output
 
 class Info(mlbgame.object.Object):
-    """Holds information about the league or teams"""
+    """Holds information about the league or teams
+    
+    Properties:
+    club
+    club_common_name
+    club_common_url
+    club_full_name
+    club_id
+    club_spanish_name
+    dc_site
+    display_code
+    division
+    es_track_code
+    esp_common_name
+    esp_common_url
+    facebook
+    facebook_es
+    fanphotos_url
+    fb_app_id
+    field
+    google_tag_manager
+    googleplus_id
+    historical_team_code
+    id
+    instagram
+    instagram_id
+    league
+    location
+    medianet_id
+    mobile_es_url
+    mobile_short_code
+    mobile_url
+    mobile_url_base
+    name_display_long
+    name_display_short
+    newsletter_category_id
+    newsletter_group_id
+    photostore_url
+    pinterest
+    pinterest_verification
+    pressbox_title
+    pressbox_url
+    primary
+    primary_link
+    secondary
+    snapchat
+    snapchat_es
+    team_code
+    team_id
+    tertiary
+    timezone
+    track_code
+    track_filter
+    tumblr
+    twitter
+    twitter_es
+    url_cache
+    url_esp
+    url_prod
+    vine
+    """
 
     def nice_output(self):
         """Return a string for printing"""
