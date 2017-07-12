b2c737f89d4917275c51692b778917048045b302
Zach Panzarino <zachary@panzarino.com>
2017-07-12 22:33:47 +0000

    Update version and statmap tests

--- tests/test_statmap.py
+++ tests/test_statmap.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 import unittest
-import mlbgame.statmap
 
 class TestStatmap(unittest.TestCase):
+
     def test_statmap(self):
+        import mlbgame.statmap
         statmap = mlbgame.statmap.idmap
         self.assertIsInstance(statmap, dict)
--- tests/test_version.py
+++ tests/test_version.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 
 import unittest
-import mlbgame.version
 
 class TestVersion(unittest.TestCase):
-    def test_version(self):
+
+    def test_version_module(self):
+        import mlbgame.version
         version = mlbgame.version.__version__
+        self.assertIsInstance(version, str)
+
+    def test_version_variable(self):
+        import mlbgame
+        version = mlbgame.VERSION
         self.assertIsInstance(version, str)
